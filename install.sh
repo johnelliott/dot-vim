ce10c67d85400c81a54da29db2aead1373802331
John Elliott <johnelliott703@gmail.com>
2017-01-09 21:25:46 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -6,17 +6,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-read -p "Would you like to clone base-16-vim default colors? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  curl -fLo ~/.vim/colors/base16-default-dark.vim --create-dirs \
-    https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-default-dark.vim
-  curl -fLo ~/.vim/colors/base16-default-light.vim --create-dirs \
-    https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-default-light.vim
-fi
-
 read -p "Would you like to copy .vimrc? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  cp .vimrc ~
+  cp .vimrc ~/.vimrc
 fi
